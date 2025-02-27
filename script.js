6238a2039469f6d656569a8af842f194f24d15a0
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-27 11:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function delay(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000);
+	})
+}
+
+delay.then((arr)=>{
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			const even = arr.filter(num => num%2===0)
+			document.getElementById("output").innerHTML=JSON.stringify(evens);
+			resolve(even);
+		},1000);
+	}
+})
